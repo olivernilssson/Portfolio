ce97a9b4f9442cf6550d81aac29c9cbdd567f2b9
Oliver Nilsson <olivernilsson@live.se>
2023-10-25 01:48:25 +0200

    Update content on start page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -260,13 +260,10 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Bridging Finance and Technology.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Oliver, currently studying Master of Science in Finance at the Business School of Gothenburg University. I have a passion for finance and technology and I am looking for a position where I can combine these two interests.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
@@ -285,7 +282,7 @@ export default async function Home() {
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="www.linkedin.com/in/oliver-nilsson-b994641aa"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
