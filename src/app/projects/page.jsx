a8daffb21f44bb8dca6e2167cc3cdbdeb3914ff2
Oliver Nilsson <olivernilsson@live.se>
2023-11-07 21:39:23 +0100

    First beta version of custom project component

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,6 +7,8 @@ import logoCosmos from '@/images/logos/cosmos.svg'
 import logoHelioStream from '@/images/logos/helio-stream.svg'
 import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
 import logoSons from '@/images/logos/sons.svg'
+import { getAllProjects } from '@/lib/projects'
+import { formatDate } from '@/lib/formatDate'
 
 const projects = [
   {
@@ -62,7 +64,8 @@ export const metadata = {
   description: 'Things I’ve made trying to put my dent in the universe.',
 }
 
-export default function Projects() {
+export default async function Projects() {
+  let projects = await getAllProjects()
   return (
     <SimpleLayout
       title="Things I’ve made trying to put my dent in the universe."
@@ -73,7 +76,7 @@ export default function Projects() {
         className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
       >
         {projects.map((project) => (
-          <Card as="li" key={project.name}>
+          <Card as="li" key={project.slug}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image
                 src={project.logo}
@@ -82,14 +85,22 @@ export default function Projects() {
                 unoptimized
               />
             </div>
-            <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href}>{project.name}</Card.Link>
+            <h3 className="mt-3 text-base text-zinc-800 dark:text-zinc-100">
+              <Card.Eyebrow
+                as="time"
+                dateTime={project.date}
+                className="mt-1 hidden md:block"
+              >
+                {formatDate(project.date)}
+              </Card.Eyebrow>
+            </h3>
+            <h2 className="mt-0 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+              <Card.Link href={project.slug}>{project.title}</Card.Link>
             </h2>
             <Card.Description>{project.description}</Card.Description>
-            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-orange-500 dark:text-zinc-200">
-              <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
-            </p>
+            <div>
+              <Card.Cta>View project</Card.Cta>
+            </div>
           </Card>
         ))}
       </ul>
