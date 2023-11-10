10dd9213279b1027f951e6267d84cdd2bcd39353
Oliver Nilsson <olivernilsson@live.se>
2023-11-10 20:56:45 +0100

    New counter and add change in percentage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,7 +22,7 @@ import image5 from '@/images/photos/image-5.jpg'
 import { getAllProjects } from '@/lib/projects'
 import { formatDate } from '@/lib/formatDate'
 import { VisitTracker } from '@/components/VisitTracker'
-import { countTotalVisits } from '@/lib/queries'
+import { calculateDailyChange, calculateWeeklyChange, countTotalVisits, countVisitsThisWeek, countVisitsToday } from '@/lib/queries'
 
 
 export function MailIcon(props) {
@@ -165,13 +165,32 @@ async function LatestProjects() {
 }
 
 async function DisplayVisits() {
-  let counter = await countTotalVisits()
-  return (
-    <p className="text-sm text-zinc-400 dark:text-zinc-500">
-      <VisitTracker />
-    {counter} visits
-    </p>
-  )
+  let counter = await countVisitsThisWeek()
+  let difference = await calculateWeeklyChange()
+  return <div>
+  <VisitTracker />
+  <div className="rounded-2xl border border-zinc-300 dark:border-zinc-700/40">
+    <div className="px-4 py-5 sm:p-6">
+    <dl>
+      <div className="flex items-center justify-between">
+      <dt className="text-sm font-semibold text-zinc-900 dark:text-zinc-100">Total visitors this week</dt>
+      <svg className={difference < 0 ? "w-6 h-6 fill-current text-red-500 transform rotate-180" : "w-6 h-6 fill-current text-green-700"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+          fill="currentColor">
+          <path fillRule="evenodd"
+              d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+              clipRule="evenodd" />
+      </svg>
+      </div>
+      <div className="flex items-center justify-between">
+          <dd className="mt-1 text-3xl leading-9 font-semibold text-orange-500">{counter}</dd>
+          <span className={difference < 0 ? "text-red-500 text-sm font-semibold ml-2" : "text-green-500 text-sm font-semibold ml-2"}>
+              {difference > 0 ? `+${difference}` : difference}%
+          </span>
+      </div>
+      </dl>
+    </div>
+  </div>
+</div>
 }
 
 function Resume() {
@@ -210,7 +229,7 @@ function Resume() {
   ]
 
   return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+    <div className="rounded-2xl border border-zinc-300 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Work Experience</span>
@@ -259,7 +278,7 @@ export default async function Home() {
   return (
     <>
       <Container className="mt-9">
-        <div className="flex">
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-[3fr,1fr]">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Bridging Finance and Technology.
--- src/lib/queries.js
+++ src/lib/queries.js
@@ -23,3 +23,27 @@ export async function countVisitsToday() {
     const results = await conn.execute('select count (*) as count from visit where date(created_at) >= curdate()')
     return results.rows[0].count
 }
+
+export async function countVisitsThisWeek() {
+    const conn = connect(config)
+    const results = await conn.execute('select count (*) as count from visit where YEARWEEK(created_at) = YEARWEEK(NOW())')
+    return results.rows[0].count
+}
+
+// Calculate the difference from the previous week in percent
+export async function calculateWeeklyChange() {
+    const conn = connect(config)
+    const results = await conn.execute('select count (*) as count from visit where YEARWEEK(created_at) = YEARWEEK(NOW()) - 1')
+    const previousWeek = results.rows[0].count
+    const currentWeek = await countVisitsThisWeek()
+    return Math.round(((currentWeek - previousWeek) / previousWeek) * 100)
+}
+
+// Calculate the difference from the yesterday in percent
+export async function calculateDailyChange() {
+    const conn = connect(config)
+    const results = await conn.execute('select count (*) as count from visit where date(created_at) = curdate() - 1')
+    const previousDay = results.rows[0].count
+    const currentDay = await countVisitsToday()
+    return Math.round(((currentDay - previousDay) / previousDay) * 100)
+}
