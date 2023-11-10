5939bcde017fc88c8530e41d365a541f34d5714e
Oliver Nilsson <olivernilsson@live.se>
2023-11-10 17:38:24 +0100

    Try without any visit tracking enabled

--- src/components/VisitTracker.jsx
+++
@@ -1,5 +0,0 @@
-import { createVisit } from '@/lib/queries'
-
-export async function VisitTracker() {
-    await createVisit()
-}
--- src/lib/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
-export const config = {
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD
-}
