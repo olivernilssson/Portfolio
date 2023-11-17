767ab66df77f86d18eec06e617ed364f64645b3d
Oliver Nilsson <olivernilsson@live.se>
2023-11-17 18:16:36 +0100

    New test logs

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,9 +1,9 @@
 import glob from 'fast-glob'
 
 async function importProject(projectFilename) {
+  console.log("4")
   let { project } = await import(`../app/projects/${projectFilename}`)
-  console.log("test")
-  console.log(project.title)
+  console.log("5")
 
   return {
     slug: projectFilename.replace(/(\/page)?\.mdx$/, ''),
@@ -12,12 +12,12 @@ async function importProject(projectFilename) {
 }
 
 export async function getAllProjects() {
-  console.log("test getAllProjects")
+  console.log("0 test getAllProjects")
   let projectFilenames = await glob('*/page.mdx', {
     cwd: './src/app/projects',
   })
-
+  console.log("1")
   let projects = await Promise.all(projectFilenames.map(importProject))
-
+  console.log("2")
   return projects.sort((a, z) => +new Date(z.date) - +new Date(a.date))
 }
